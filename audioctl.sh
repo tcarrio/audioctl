244b51b77484e12b86191fdcaaf6c94d6dd42ed2
tcarrio <tom@carrio.me>
2016-08-31 14:28:12 -0400

    Autodetection based on sourced variables _DESC

--- audioctl.sh
+++ audioctl.sh
@@ -3,37 +3,46 @@
 #	I don't like pulling up settings to do this so here's a short script 
 # Obviously the indices listed only apply to my system
 
-# set output indices
-while read -r line
-do
-	if [[ $line == "Sink #"* ]]
-	then
-		tmp_out="${line:-1}"
-		echo "$tmp_out"
-	else if [[ $line == "device.description" ]]
-	then
-		tmp_desc="$(echo $line | cut -d "=" -f 2)"
-		case $tmp_desc in
-			*$HEADPHONE_DESC*)
-				HEADPHONES=$tmp_out
-				;;
-			*$SPEAKERS_DESC*)
-				SPEAKERS=$tmp_out
-				;;
-		esac
 
-		# or use ifs?
-		if [[ $tmp_desc == "$HEADPHONE_DESC" ]]
-		then
-			HEADPHONES=$tmp_out
-		else if [[ $(echo $line | 
-	fi
-done < <(pactl list sinks | grep -e "Sink #" -e "device.description")
-
+# set preliminary values for output devices
+# these can be different after reboots, so we'll check
+# for the correct values as well
 HEADPHONES="4"
 SPEAKERS="1"
+
+source outputs.conf
+#echo "$HEADPHONES_DESC"
+#echo "$SPEAKERS_DESC"
 # `pactl list sinks` to retrieve card info
 # only the index is required for moving audio
+# set output indices
+
+tmp_out=""
+tmp_desc=""
+
+while read -r line
+do
+	#echo "$line"
+	if [[ $line == "Sink #"* ]]
+	then
+		tmp_out="$(echo $line|rev|cut -c 1|rev)"
+		#printf "Got tmp_out! Sink:%s\n" $tmp_out
+	elif [[ $line == "device.description"* ]]
+	then
+		tmp_desc="$(echo $line | cut -d "=" -f 2)"
+		#printf "Sink:%1s\tDesc:%s\n" "$tmp_out" "$tmp_desc"
+		case $tmp_desc in
+			*$HEADPHONES_DESC*)
+				HEADPHONES="$tmp_out"
+				#printf "Set headphones to sink %s\n" $HEADPHONES
+				;;
+			*$SPEAKERS_DESC*)
+				SPEAKERS="$tmp_out"
+				#printf "Set speakers to sink %s\n" $SPEAKERS
+				;;
+		esac
+	fi
+done < <(pactl list sinks | grep -e "Sink #" -e "device.description")
 
 # show help 
 usage(){
@@ -51,10 +60,10 @@ main(){
 	if [ -z "$1" ];then
 		usage
 	fi
-	pactl list short sink-inputs
+	#pactl list short sink-inputs
 	for snd in $(pactl list short sink-inputs | awk '{print $1;}' )
 	do 
-		echo "$snd"
+		#echo "$snd"
 		pactl move-sink-input $snd $1 1>/dev/null 2>/dev/null
 		pactl set-default-sink $1 1>/dev/null 2>/dev/null
 	done
@@ -82,6 +91,6 @@ case "$1" in
 		;;
 esac
 
-echo "$output"
+#echo "$output"
 main $output
 	
